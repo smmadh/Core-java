c97759b5e8b81598c6c30941a2ccc2dab6ec7e46
smmadh <139976937+smmadh@users.noreply.github.com>
2023-11-20 17:49:32 +0530

    Add files via upload

---
+++ conditionalop.java
@@ -0,0 +1,18 @@
+package operator;
+
+public class conditionalop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=20;
+		int b=30;
+		int min;
+		int max;
+		min=(a<b)?a:b;
+		max=(a>b)?a:b;
+		System.out.println("max value is:"+min);
+		System.out.println("max value is:"+max);
+
+	}
+
+}
